231bcbde7dbd511a61e4af85e6f2a5556c845f91
potentiallycold <potentiallycold@gmail.com>
2025-06-27 05:10:59 +0530

    added firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,12 @@
+// firebase-init.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBW7hA7uOLrgb8HdIHVtC885leX8MSb8AU",
+  authDomain: "pitchbuddy-app.firebaseapp.com",
+  projectId: "pitchbuddy-app",
+  storageBucket: "pitchbuddy-app.firebasestorage.app",
+  messagingSenderId: "466259946031",
+  appId: "1:466259946031:web:4dad01be7977df0c0674da"
+};
+
+firebase.initializeApp(firebaseConfig);
